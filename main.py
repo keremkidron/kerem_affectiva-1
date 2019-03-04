0acd7a26438cd2f99dd88f1c7cc79b875344ab0f
Goren Gordon <goren@gorengordon.dom>
2019-03-04 15:24:14 +0200

    added main

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
 import json
 
-x = json.load((open('data/1024.0.json')))
-print(x.keys())
+subject_id = 1024
+
+x = json.load((open('data/%d.0.json' % subject_id)))
+# x[step_id] = (time, affectiva)
+
+# example:
+step_id = '0'
+time_step = 0
+time_stamp = x[step_id][time_step][0]
+aff = x[step_id][time_step][1]
+expression = aff['expressions']
+emotions = aff['emotions']
+print(time_stamp)
+print(emotions)
