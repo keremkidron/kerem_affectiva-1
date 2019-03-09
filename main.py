dec6e70422aa965dc5303b904116829d831c4ab4
keremkidron <48252953+keremkidron@users.noreply.github.com>
2019-03-09 12:53:07 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import json
+import matplotlib.pyplot as plt
 
 subject_id = 1024
 
@@ -13,4 +14,12 @@ aff = x[step_id][time_step][1]
 expression = aff['expressions']
 emotions = aff['emotions']
 print(time_stamp)
-print(emotions)
+print(emotions[7])
+
+x_axis = [x[step_id][i][0] for i in range(len(x))]
+y_axis = [x[step_id][i][1]['emotions'][7] for i in range(len(x))]
+
+print(x_axis)
+print(y_axis)
+plt.plot(x_axis,y_axis)
+plt.show() 
